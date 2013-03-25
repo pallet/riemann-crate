b863b4db232a430f47d279f2b20992ea855d4b9a
Hugo Duncan <hugo@hugoduncan.org>
2013-03-25 17:06:55 -0400

    Update for service abstraction

--- test/pallet/crate/riemann_test.clj
+++ test/pallet/crate/riemann_test.clj
@@ -4,11 +4,22 @@
    [pallet.actions :refer [package-manager]]
    [pallet.algo.fsmop :refer [complete?]]
    [pallet.api :refer [plan-fn server-spec]]
-   [pallet.crate.java :refer [java]]
-   [pallet.crate.network-service :refer [wait-for-port-listen]]))
+   [pallet.crate.java :as java]
+   [pallet.crate.network-service :refer [wait-for-port-listen]]
+   [pallet.crate.nohup :as nohup]
+   [pallet.crate.runit :as runit]))
 
-(def live-test-spec
+(def live-nohup-test-spec
   (server-spec
-   :extends [(java {}) (riemann/server-spec {})]
+   :extends [(java/server-spec {})
+             (riemann/server-spec {:supervisor :nohup})]
+   :phases {:install (plan-fn (package-manager :update))
+            :test (plan-fn (wait-for-port-listen 5555))}))
+
+(def live-runit-test-spec
+  (server-spec
+   :extends [(java/server-spec {})
+             (runit/server-spec {})
+             (riemann/server-spec {:supervisor :runit})]
    :phases {:install (plan-fn (package-manager :update))
             :test (plan-fn (wait-for-port-listen 5555))}))
