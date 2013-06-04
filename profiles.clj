bc9c5e162537bb25a988ea172bc80f056b3cc122
Hugo Duncan <hugo@hugoduncan.org>
2013-06-04 13:23:36 -0400

    Add jclouds and vmfest profiles

--- profiles.clj
+++ profiles.clj
@@ -35,4 +35,14 @@
  :pallet {:jvm-opts ["-Djna.nosys=true"]}
  :release
  {:set-version
-  {:updates [{:path "README.md" :no-snapshot true}]}}}
+  {:updates [{:path "README.md" :no-snapshot true}]}}
+ :jclouds {:repositories
+           {"sonatype"
+            "https://oss.sonatype.org/content/repositories/releases/"}
+           :dependencies [[org.cloudhoist/pallet-jclouds "1.5.2"]
+                          [org.jclouds/jclouds-allblobstore "1.5.5"]
+                          [org.jclouds/jclouds-allcompute "1.5.5"]
+                          [org.jclouds.driver/jclouds-slf4j "1.5.5"
+                           :exclusions [org.slf4j/slf4j-api]]
+                          [org.jclouds.driver/jclouds-sshj "1.5.5"]]}
+ :vmfest {:dependencies [[com.palletops/pallet-vmfest "0.3.0-alpha.4"]]}}
