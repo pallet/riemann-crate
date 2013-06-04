25984433fa6377fd4f3f8b30f2ccd99a13044e4a
Hugo Duncan <hugo@hugoduncan.org>
2013-06-04 13:43:03 -0400

    Add prep tasks

--- project.clj
+++ project.clj
@@ -4,4 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.palletops/pallet "0.8.0-beta.6"]])
+                 [com.palletops/pallet "0.8.0-beta.9"]]
+  :resource {:resource-paths ["doc-src"]
+             :target-path "target/classes/pallet_crate/riemann_crate/"
+             :includes [#"doc-src/USAGE.*"]}
+  :prep-tasks ["resource" "crate-doc"])
