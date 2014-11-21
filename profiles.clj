b45b065407cf2bfd380c4c2f863375b0cafefed8
Hugo Duncan <hugo@hugoduncan.org>
2014-11-21 15:17:27 -0500

    Use lein-pallet-release

--- profiles.clj
+++ profiles.clj
@@ -8,10 +8,8 @@
                  [ch.qos.logback/logback-classic "1.0.9"]]
   :plugins [[com.palletops/pallet-lein "0.8.0-alpha.1"]
             [com.palletops/lein-pallet-crate "0.1.0"]
-            [lein-set-version "0.3.0"]
             [lein-resource "0.3.2"]
-            [codox/codox.leiningen "0.6.4"]
-            [lein-marginalia "0.7.1"]]
+            [lein-pallet-release "RELEASE"]]
   :aliases {"live-test-up"
             ["pallet" "up"
              "--phases" "install,configure,test"
@@ -22,21 +20,7 @@
                    :live-test :live-test
                    :all (constantly true)}
   :repositories {"boundary-site" "http://maven.boundary.com/artifactory/repo"}}
- :doc {:dependencies [[com.palletops/pallet-codox "0.1.0"]]
-       :plugins [[codox/codox.leiningen "0.6.4"]
-                 [lein-marginalia "0.7.1"]]
-       :codox {:writer codox-md.writer/write-docs
-               :output-dir "doc/0.8/api"
-               :src-dir-uri "https://github.com/pallet/riemann-crate/blob/develop"
-               :src-linenum-anchor-prefix "L"}
-       :aliases {"marg" ["marg" "-d" "doc/0.8/annotated"]
-                 "codox" ["doc"]
-                 "doc" ["do" "codox," "marg"]}
-       }
  :pallet {:jvm-opts ["-Djna.nosys=true"]}
- :release
- {:set-version
-  {:updates [{:path "README.md" :no-snapshot true}]}}
  :jclouds {:repositories
            {"sonatype"
             "https://oss.sonatype.org/content/repositories/releases/"}
